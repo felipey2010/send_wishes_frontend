5ed0c3e69914f9e2730cd122d58fc68aeefb7c3a
felipey2010 <mahamaphilip@yahoo.com>
2022-03-17 16:19:00 -0400

    modified project in general

--- src/components/FloatingButton/index.js
+++ src/components/FloatingButton/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Fab from "@material-ui/core/Fab";
@@ -60,66 +59,3 @@ export default function FloatingButton({ getCards }) {
     </React.Fragment>
   );
 }
-=======
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Fab from "@material-ui/core/Fab";
-import AddIcon from "@material-ui/icons/Add";
-import { Bounce } from "./FloatingButtonElements";
-import CreatePost from "../CreatePost";
-import Tooltip from "@material-ui/core/Tooltip";
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    "& > *": {
-      position: "fixed",
-      bottom: theme.spacing(2),
-      right: theme.spacing(2),
-      margin: theme.spacing(1),
-      backgroundColor: "#e0920e",
-      borderRadius: "50%",
-
-      "&:hover": {
-        backgroundColor: "#9c6302",
-        transition: "0.2s ease-out",
-      },
-    },
-  },
-  fab: {
-    backgroundColor: "#e0920e",
-    "&:hover": {
-      backgroundColor: "#9c6302",
-      transition: "0.2s ease-out",
-    },
-  },
-  extendedIcon: {
-    marginRight: theme.spacing(1),
-  },
-}));
-
-export default function FloatingButton({ getCards }) {
-  const classes = useStyles();
-  const [open, setOpen] = useState(false);
-
-  function handleOpen() {
-    setOpen(true);
-  }
-
-  return (
-    <React.Fragment>
-      {open === false && (
-        <div className={classes.root} onClick={handleOpen}>
-          <Bounce>
-            <Tooltip title="Add Card" placement="left">
-              <Fab className={classes.fab} aria-label="add">
-                <AddIcon />
-              </Fab>
-            </Tooltip>
-          </Bounce>
-        </div>
-      )}
-      {open && <CreatePost open={open} setOpen={setOpen} getCards={getCards} />}
-    </React.Fragment>
-  );
-}
->>>>>>> 1157f83e3e76d98f4283c8fb2e4ab1842298a2e2
